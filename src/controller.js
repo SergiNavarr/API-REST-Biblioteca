16a7aac8973fa5754200a4e10352c9a26b1767d9
serginavarr <nachonavarro197@gmail.com>
2023-10-17 06:47:03 -0300

    Creacion de repositorio

---
+++ src/controller.js
@@ -0,0 +1,36 @@
+import { pool } from "./database.js";
+
+class LibroController{
+    async getAll(req, res){
+        const [result] = await pool.query('SELECT * FROM libros');
+        res.json(result);
+        
+    }
+
+    async getOne(req, res){
+        const libro = req.body;
+        const id_libro = parseInt(libro.id);
+        const [result] = await pool.query(`SELECT * FROM Libros WHERE id=(?)`, [id_libro]);
+        res.json(result);
+    }
+    
+    async add(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?,?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+        res.json({"Id insertado": result.insertId});  
+    }
+
+    async delete(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`, [libro.id]);
+        res.json({"Registros eliminados": result.affectedRows});
+    }
+
+    async update(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año_publicacion=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN, libro.id]);
+        res.json({"Registros actualizados": result.changedRows});
+    }
+}
+
+export const libro = new LibroController();
